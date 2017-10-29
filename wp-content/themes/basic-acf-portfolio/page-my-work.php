dd85c90db2a1a8d8387d5abcb16e72592e2dac04
milivojev <vas.milivoj@gmail.com>
2017-10-29 11:57:48 +0100

    page page-my work changed so it displays categories and leads to them

--- wp-content/themes/basic-acf-portfolio/page-my-work.php
+++ wp-content/themes/basic-acf-portfolio/page-my-work.php
@@ -23,31 +23,36 @@
 
       <!-- Marketing Icons Section -->
       <div class="row">
+
+        <!-- 
+
+On the My Work page, don't actually want to show posts, we want to show categories.
+
+Use the get_categories() method to get all your categories into a variable. Open up the My Work page that we used as an example in class, and replace the WP_Query of posts that we did in class with foreach loop showing all categories in these boxes. It should be really easy and straightforward.
+ -->
         <?php
 
-          $myworks = new WP_Query([
-            'post_type'=>'post',
-            
-          ]);
+          $categories = get_categories( );
 
-          while($myworks->have_posts()):
-            $myworks->the_post();
-            
+         foreach($categories as $category):
+          // echo "<pre>";
+          // var_dump($category);
+          // echo "</pre>";
 
         ?>
         <div class="col-lg-4 mb-4">
           <div class="card h-100">
-            <h4 class="card-header"><?=the_title();?></h4>
+            <h4 class="card-header"><?=$category->name;?></h4>
             <div class="card-body">
-              <p class="card-text"><?= excerpt(get_the_content());?></p>
+              <p class="card-text"><?= $category->description;?></p>
             </div>
             <div class="card-footer">
-              <a href="<?= get_permalink( );?>" class="btn btn-primary">Learn More</a>
+              <a href="<?= get_category_link($category->cat_ID );?>" class="btn btn-primary">Learn More</a>
             </div>
           </div>
         </div>
         <?php
-      endwhile;
+      endforeach;
       wp_reset_postdata();
         ?>
       </div>
